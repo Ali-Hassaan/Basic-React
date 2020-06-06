03c2495700f2832c324bf0d45f043b7ba80f3258
Ali Hassaan <alihassaan0310@gmail.com>
2020-06-06 23:46:54 +0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Hi() {
+  return <div>
+            Hello <strong>CNC</strong>
+            <div>
+          <h1><i>Cities Name:</i></h1>
+          <ol>
+            <li>Karachi</li>
+            <li>Lahore</li>
+            <li>Islamabad</li>
+          </ol>
+          value of x = {152 + 48}
+        </div>  
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  </div>;
+}
+
+ReactDOM.render(<Hi/>, document.querySelector('#root'));
